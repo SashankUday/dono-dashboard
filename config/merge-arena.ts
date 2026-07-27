493d073f1ce497d772f752326623cdfafa5b8a6d
Sashank Uday <busythrone@gmail.com>
2026-07-27 17:07:35 +0100

    Minor: Updates team logs so everyone is shown on dashboard

--- config/merge-arena.ts
+++ config/merge-arena.ts
@@ -36,6 +36,21 @@ export const mergeArenaConfig = {
       active: true,
       mergeSoundFile: "public/audio/Sashank_Merging.mp3",
     },
+    jujmun: {
+      displayName: "Juyeon",
+      active: true,
+      mergeSoundFile: "public/audio/Juyeon_Merging.mp3",
+    },
+    JoesephBegg: {
+      displayName: "Joesph",
+      active: true,
+      mergeSoundFile: "public/audio/Joesph_Merging.mp3",
+    },
+    amiroopraimed: {
+      displayName: "Amrit",
+      active: true,
+      mergeSoundFile: "public/audio/Amrit_Merging.mp3",
+    },
   } satisfies Record<string, MergeArenaMember>,
   botLogins: ["dependabot[bot]", "renovate[bot]", "github-actions[bot]"],
 } as const;
